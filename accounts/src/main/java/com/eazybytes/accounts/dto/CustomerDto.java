ff477bd910bde69c8ed194c5a1342db86a84a141
james <jamboyoyo@gmail.com>
2025-08-06 11:22:54 -0400

    finished video 20. Can post to h2 DB form postman

--- accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -12,27 +12,4 @@ public class CustomerDto {
 
     private String mobileNumber;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
 }
--- accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -37,6 +37,8 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
                     + customerDto.getMobileNumber());
         }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -53,6 +55,8 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
         return newAccount;
     }
 }
