18f34131189bb0f46597a1cb4e238e0adfa54a92
james <jamboyoyo@gmail.com>
2025-08-08 11:02:27 -0400

    Added validations, completed Video 25

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -2,39 +2,23 @@ package com.eazybytes.accounts.dto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
 
+    @NotEmpty
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 
-    public Long getAccountNumber() {
-        return accountNumber;
-    }
 
-    public void setAccountNumber(Long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public String getBranchAddress() {
-        return branchAddress;
-    }
-
-    public void setBranchAddress(String branchAddress) {
-        this.branchAddress = branchAddress;
-    }
 }
