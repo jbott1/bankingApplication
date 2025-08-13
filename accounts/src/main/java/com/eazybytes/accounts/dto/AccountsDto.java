397e9cd5b34d402456d66db22651441587e6ae78
james <jamboyoyo@gmail.com>
2025-08-13 09:14:22 -0400

    Video 30 Swagger api documentation and annotations

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
@@ -7,16 +8,24 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+
+        name = "Accounts",
+        description =" Schema to hold Account information"
+)
 public class AccountsDto {
 
 
+    @Schema( description = "Account Number of Bank Account", example = "1892335615")
     @NotEmpty
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @Schema( description = "Account type of Bank Account", example = "Savings")
     @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @Schema( description = "Branch Address", example = "123 NewYork")
     @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 
